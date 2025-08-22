82587d924aad574cc670b81bf1834d51e771d310
Furla <joaofirlaferre200809@gmail.com>
2025-08-22 11:54:06 -0300

    Adiciona o Retro fit na Interface

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -1,4 +1,10 @@
 package com.example.projectcurrencyconverterapi;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 public interface ExchangeRateService {
+    /**
+     * Metodo para obter taxas de cambios recentes
+     */
 }
