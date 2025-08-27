2dd26cfebb939fa9a9231743d45a37c42a6cc05f
Furla <joaofirlaferre200809@gmail.com>
2025-08-27 09:38:24 -0300

    Adiciona metodo para conversão das moedas

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
@@ -4,6 +4,13 @@ package com.example.projectcurrencyconverterapi;
 import java.util.HashMap; // Importa a classe Map da biblioteca Java, usada para armazear pares da chave-valor
 import java.util.Map; // Importa a classe Map da biblioteca Java, usada para armazenar pares chave-valor
 public class ExchangeRatesResponse {
-    // declara um mapa que armazena taxas de conversão em moedas.
+    // declara um mapa que armazena taxas de conversão em moedas. (ex. USD, EUR, GBP)
     // A chave (String) representa o codigo da Moeda e o valor (Double) representa a taxa de conversão.
+
+    private Map<String, Double> conversion_rates = new HashMap<>();
+    // Metodo publico para acessar as taxas de conversão
+    // Retorna o mapa de taxas de conversão
+    public Map<String, Double> getConversionRates() {
+        return conversion_rates;
+    }
 }
