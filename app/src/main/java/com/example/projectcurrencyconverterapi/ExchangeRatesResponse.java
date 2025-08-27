dc86094a2a52b668acde18c9128fb9fcfd9d33bb
Furla <joaofirlaferre200809@gmail.com>
2025-08-27 09:31:01 -0300

    Importa a classe Map, declara um mapa que armazena taxas de conversão com o String e Double

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
@@ -1,5 +1,9 @@
 // modelo de dados (DTO - Data Trasfer Object) que representa a resposta json da API
 package com.example.projectcurrencyconverterapi;
 
+import java.util.HashMap; // Importa a classe Map da biblioteca Java, usada para armazear pares da chave-valor
+import java.util.Map; // Importa a classe Map da biblioteca Java, usada para armazenar pares chave-valor
 public class ExchangeRatesResponse {
+    // declara um mapa que armazena taxas de conversão em moedas.
+    // A chave (String) representa o codigo da Moeda e o valor (Double) representa a taxa de conversão.
 }
