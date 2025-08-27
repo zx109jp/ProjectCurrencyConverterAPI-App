23f152f97e0a4a141723495ba29035e674f48d80
Furla <joaofirlaferre200809@gmail.com>
2025-08-27 09:19:23 -0300

    Adiciona os path da API

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -2,9 +2,20 @@ package com.example.projectcurrencyconverterapi;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 public interface ExchangeRateService {
     /**
      * Metodo para obter taxas de cambios recentes
+
      */
+
+    $GET("/v6/{apiKey}/latest/{baseCurrency}")
+        Call<ExchangeRatesResponse> getExchangeRates(
+
+        @Path("apiKey") String apiKey,
+    // Substitui {apiKey} na URL com valor fornecido
+        @Path("baseCurrency") String baseCurrency,
+
+    // Subtitui {baseCurrency} na URL com valor fornecido
+        );
 }
