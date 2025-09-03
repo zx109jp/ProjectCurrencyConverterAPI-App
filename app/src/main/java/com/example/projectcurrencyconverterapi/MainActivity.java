ed4bcc6c3c09be3dcdff78bb28e41eb111268d9f
Furla <joaofirlaferre200809@gmail.com>
2025-09-03 09:24:06 -0300

    Cria Import's e classes Privadas como o Spinner

--- app/src/main/java/com/example/projectcurrencyconverterapi/MainActivity.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/MainActivity.java
@@ -1,4 +1,33 @@
 package com.example.projectcurrencyconverterapi;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Map;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+//import
+import com.example.projectcurrencyconverterapi.ExchangeRateService;
+import com.example.projectcurrencyconverterapi.RetrofitClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerMoedaOrigem, spinnerMoedaDestino;
+
+    private Button buttonConverter;
+
+    private TextView tvResultado;
+
+    private List<String> moedas = List.of("USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "HKD", "NZD");
+
+    private Map<String, Double> taxasDeCambio;
 }
