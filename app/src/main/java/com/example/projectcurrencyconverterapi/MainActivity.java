87536312635091387a782aae8ff1898c0dbecd9e
Furla <joaofirlaferre200809@gmail.com>
2025-09-10 09:41:02 -0300

    Inicializa os componentes da interface

--- app/src/main/java/com/example/projectcurrencyconverterapi/MainActivity.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Spinner spinnerMoedaOrigem, spinnerMoedaDestino;
 
-    private Button buttonConverter;
-
+    private Button btnConverter;
+    private EditText etValor;
     private TextView tvResultado;
 
     private List<String> moedas = List.of("USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "HKD", "NZD");
@@ -34,6 +34,33 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Define o layout da atividade
+
+        // Inicializa os componentes da interface
+
+        spinnerMoedaOrigem = findViewById(R.id.spinnerMoedaOrigem);
+        spinnerMoedaDestino = findViewById(R.id.spinnerMoedaDestino);
+        etValor = findViewById(R.id.etValor);
+        btnConverter = findViewById(R.id.btnConverter);
+        tvResultado = findViewById(R.id.tvResultado);
+
+        // Configura o Spinner de Moedas de Origem
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, moedas);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMoedaOrigem.setAdapter(adapter);
+        spinnerMoedaDestino.setAdapter(adapter);
+
+        // Carrega as taxas de cambio da API
+
+        // Configura o botão de conversã
+        btnConverter.setOnClickListener(view -> converterMoeda());
+
+    }
+// Metodo para carregar taxas de cambio da API
+private void carregarTaxasDeCambio(){
+}
+// Metodo para converter o valor de uma moeda para outra
+private void converterMoeda(){
+
 }
 }
